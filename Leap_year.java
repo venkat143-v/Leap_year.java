a7c4d9934fa5cca9433809dfbc0648d0815c726a
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-17 17:07:30 +0530

    Add files via upload

---
+++ Leap_year.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Leap_year{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Year :");
+		int year = sc.nextInt(); // 2013
+if(((year % 4 ==0) && (year % 100 != 0) )|| (year % 400 == 0))
+System.out.println("Leap Year = "+ year);
+else
+	System.out.println("Not Leap Year = "+ year);
+	}
+}
